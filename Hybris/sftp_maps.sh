99adb74f4d43166851ba2d517d81fcdc21562448
rbarbosa-inetum <91738714+rbarbosa-inetum@users.noreply.github.com>
2022-06-30 03:50:31 +0100

    Create sftp_maps.sh

---
+++ Hybris/sftp_maps.sh
@@ -0,0 +1,6 @@
+mount --bind /efs/interfaces/IFU /home/ifu/IFU
+mount --bind /efs/interfaces/CSS /home/ifu/CSS
+mount --bind /efs/maj-properties/conf /home/maj-properties/conf
+mount --bind /efs/maj-properties/a_faire /home/maj-properties/a_faire
+mount --bind /efs/interfaces /home/eai/interfaces
+mount --bind /efs/interfaces /home/sftphybris/interfaces
