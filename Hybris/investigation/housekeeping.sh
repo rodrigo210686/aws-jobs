861b2f6a62d4abe2ed1db9c1811f781d48cc8fff
rbarbosa-inetum <91738714+rbarbosa-inetum@users.noreply.github.com>
2022-12-16 17:55:25 +0000

    Create housekeeping.sh

---
+++ Hybris/investigation/housekeeping.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+
+truncate -s 0 /home/wwwroot/seb/logs/access-t*
+truncate -s 0 /home/wwwroot/seb/logs/access-m*
+truncate -s 0 /home/wwwroot/seb/logs/access-k*
+truncate -s 0 /home/wwwroot/seb/logs/access-r*
+truncate -s 0 /home/wwwroot/seb/logs/access-s*
+truncate -s 0 /home/wwwroot/seb/logs/access-a*
