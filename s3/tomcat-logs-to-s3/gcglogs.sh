526d57476d5aff487dc9767f9f0a3ce1857d7cc5
rbarbosa-inetum <91738714+rbarbosa-inetum@users.noreply.github.com>
2022-12-21 09:09:51 +0000

    Create gcglogs.sh

---
+++ s3/tomcat-logs-to-s3/gcglogs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#### V4
+timestamp()
+{
+ date +"%Y-%m-%d %T"
+}
+instance_id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+env=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" --region=eu-west-1 --query 'Tags[?Key==`environment`].Value' --output text)
+instance_name=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" --region=eu-west-1 --query 'Tags[?Key==`Name`].Value' --output text)
+gclogsDirectoryBase=/home/hybris/hybris/log/tomcat
+gclogsDirectory=${gclogsDirectoryBase}
+LOG_FILE="/var/log/collector_s3_upload.log"
+exec > >(tee -a $LOG_FILE) # directs stdout to log file
+exec 2>&1 # and also to console
+
+#export AWS_ACCESS_KEY_ID=$HEAPDUMP_UP_AWS_ACCESS_KEY_ID
+#export AWS_SECRET_ACCESS_KEY=$HEAPDUMP_UP_AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=eu-west-1
+
+NOW=$(date +"%Y%m%d")
+expirationDate=$(date -d $(date +"%Y/%m/%"d)+" 1 days" +%Y/%m/%d)
+
+echo "$(timestamp): look for cglogs to upload "
+
+cd ${gclogsDirectory}
+mkdir gclogs-$NOW
+echo "$(timestamp): Processing gclogs-$NOW"
+cp java_gc.* gclogs-$NOW
+tar -zcvf gclogs-${NOW}.tar.gz gclogs-${NOW}
+aws s3 cp gclogs-$NOW.tar.gz "s3://hybrisapplog/${env}/gclogs2/${instance_name}/${instance_name}_${instance_id}_${NOW}.tar.gz" --expires $expirationDate --acl bucket-owner-full-control
+rm -rf  gclogs-$NOW*
+echo "$(timestamp): upload gclogs successfuly"
