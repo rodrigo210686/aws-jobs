cf54c6fcff2670a8f3a0ed2cf9533c1a93911c42
rbarbosa-inetum <91738714+rbarbosa-inetum@users.noreply.github.com>
2023-01-11 12:30:43 +0000

    Update deregister-ami.sh

--- scripts-aws/deregister-ami.sh
+++ scripts-aws/deregister-ami.sh
@@ -1,7 +1,7 @@
-### When you try to delete snapshots from console, sometimes you receive a error informing that this snapshot is associated to a AMI. For delete this snapshot you have to delete the AMI.
-
-
 #/bin/bash
+### When you try to delete snapshots from console, sometimes you receive a error informing that this snapshot is associated to a AMI. For delete this snapshot you have to delete the AMI.
+# To delete, get the list of AMI showed in error, check if does not use this AMI anymore, and input the list of AMI that you want to deregister inside a file ami.txt in the same folder from this scritp.
+# You can perform it form Amazon CLI
 
 while read p; do
  aws ec2 deregister-image --image-id "$p"
