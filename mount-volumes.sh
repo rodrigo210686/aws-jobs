0d11ced23012e8cf0595a955169b2fc4c7f4bfd4
rbarbosa-inetum <91738714+rbarbosa-inetum@users.noreply.github.com>
2023-03-24 09:57:27 +0000

    Update mount-volumes.sh

--- mount-volumes.sh
+++ mount-volumes.sh
@@ -1,4 +1,9 @@
 Em alguns casos ao tentar montar um volume é apresentado erro pois os UID dos discos são iguais.
 Para montar use mo comando abaixo
-
+https://serverfault.com/questions/948408/mount-wrong-fs-type-bad-option-bad-superblock-on-dev-xvdf1-missing-codepage
 # mount -t xfs -o nouuid /dev/nvme1n1 /mnt
+
+
+# Volumes novos
+https://www.tecmint.com/add-new-disk-to-an-existing-linux/
+
