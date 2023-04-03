3fc669570dd9c5e65453c9dc1788c20040f38bde
rbarbosa-inetum <91738714+rbarbosa-inetum@users.noreply.github.com>
2023-04-03 14:54:46 +0100

    Create for-windows.ps1

---
+++ s3/for-windows.ps1
@@ -0,0 +1,3 @@
+Copy-S3Object -BucketName squash-prod-bkp-exports -Key WindowsSensor.LionLanner.exe -LocalFolder c:\Installations
+
+#https://docs.aws.amazon.com/powershell/latest/reference/items/Copy-S3Object.html
