0181e1eb058c94c779c36f0ce8384b4194f87db0
rbarbosa-inetum <91738714+rbarbosa-inetum@users.noreply.github.com>
2023-08-30 10:04:11 +0100

    Create sft-clean.sh

---
+++ crontabs/sft-clean.sh
@@ -0,0 +1,5 @@
+find /home/eai/interfaces/DynamicPricing/archive/* -mtime +180 -name "*" -exec rm -f {} \;
+find /home/eai/interfaces/095/int/archive/* -mtime +180 -name "*" -exec rm -f {} \;
+find /home/eai/interfaces/061/int/archive/* -mtime +180 -name "*" -exec rm -f {} \;
+find /home/eai/interfaces/PFS/095/int/archive/* -mtime +180 -name "*" -exec rm -f {} \;
+find /home/eai/interfaces/master/int/archive/* -mtime +180 -name "*" -exec rm -f {} \;
